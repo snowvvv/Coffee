2d79ea8ceea5c5b177fd89a388a7f2ae3304be9e
Богдан Бухольцев <buholcev@MacBook-Pro-Buholcev.local>
2024-04-24 16:40:19 +0500

    статистика

--- project/main.py
+++ project/main.py
@@ -21,12 +21,17 @@ def index():
     return render_template('index.html')
 
 
+@main.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
 @main.route('/lectures')
 @login_required
 def lectures():
     q = request.args.get('q')
     if q:
-        lectures = models.Lecture.query.filter(models.Lecture.tag.contains(f'{q}'))
+        lectures = models.Lecture.query.filter(models.Lecture.name.contains(f'{q}'))
     else:
         lectures = models.Lecture.query.all()
     return render_template('lectures.html', data=lectures)
@@ -45,35 +50,40 @@ def courses_lectures():
         return render_template('courses_lectures.html', data=lectures)
 
 
-@main.route('/lecture/<int:id>', methods=['GET', 'POST'])
+@main.route('/lectures/<int:id>', methods=['GET', 'POST'])
 @login_required
 def lecture(id):
-    lecture = models.Lecture.query.filter_by(id=id)
+    lecture = models.Lecture.query.filter_by(id=id).first()
     if request.method == 'GET':
-
         return render_template('lecture.html', data=lecture)
     else:
-        setattr(current_user, 'completed_lectures', current_user.completed_lectures + lecture.name)
-        db.session.commit()
+        if current_user.completed_lectures:
+            name = current_user.completed_lectures + ', ' + lecture.name
+            setattr(current_user, 'completed_lectures', name)
+            db.session.commit()
+        else:
+            setattr(current_user, 'completed_lectures', lecture.name)
+            db.session.commit()
+        return redirect('/lectures')
 
 
-@main.route('/create_lectures', methods=['GET', 'POST'])
+@main.route('/create_lecture', methods=['GET', 'POST'])
 @login_required
-def create_lectures():
+def create_lecture():
     if current_user.grade == 4:
         if request.method == 'POST':
-            text = request.form.get('text')
             name = request.form.get('name')
+            text = request.form.get('text')
             result = models.Lecture(
-                text=text,
                 name=name,
+                text=text,
             )
             db.session.add(result)
             db.session.commit()
-            return redirect(f'/lecture/{id}')
+            return redirect(f'/lectures')
 
         else:
-            return render_template('create_lectures.html')
+            return render_template('create_lecture.html')
     else:
         return redirect(url_for('main.index'))
 
@@ -98,13 +108,21 @@ def create_course():
 @main.route('/statistic', methods=['GET', 'POST'])
 @login_required
 def statistic():
-    user = current_user
     if request.method == 'GET':
-        render_template(
-            'statistic.html',
-            data=current_user.completed_lectures,
-            data2=current_user.completed_tests
-        )
+        users = models.User.query.all()
+        match current_user.grade:
+            case 0:
+                users = None
+            case 1:
+                users = None
+            case 2:
+                users = models.User.query.filter(models.User.grade < 2)
+            case 3:
+                users = models.User.query.filter(models.User.grade < 3)
+            case 4:
+                users = models.User.query.filter(models.User.grade < 4)
+        return render_template('statistic.html', data1=current_user, data2=users)
+
     else:
         return redirect(f'/lalka_statistic/{id}')
 
